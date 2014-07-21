34b876a2e31a2452df16d696108aa5f2f2f9575b
v3ga <julien@v3ga.net>
2014-07-21 17:38:07 +0200

    First commit

---
+++ src/tool.cpp
@@ -0,0 +1,361 @@
+//
+//  tool.cpp
+//  A_P_P_A_R_E_L
+//
+//  Created by Julien on 30/05/2014.
+//
+//
+
+#include "tool.h"
+#include "ofEventUtils.h"
+
+//--------------------------------------------------------------
+tool::tool(string id, toolManager* parent)
+{
+	m_id = id;
+	mp_toolManager = parent;
+	mp_canvas = 0;
+}
+
+//--------------------------------------------------------------
+tool::~tool()
+{
+	delete mp_canvas;
+}
+
+//--------------------------------------------------------------
+void tool::createControls(string fontName, ofVec2f posCanvas, ofVec2f dimCanvas)
+{
+   	mp_canvas = new ofxUICanvas(posCanvas.x,posCanvas.y,dimCanvas.x,dimCanvas.y);
+	mp_canvas->setFont(fontName);
+    mp_canvas->setTriggerWidgetsUponLoad(true);
+	//mp_canvas->setRetinaResolution();
+
+   createControlsCustom();
+
+   ofAddListener(mp_canvas->newGUIEvent, this, &tool::handleEvents);
+}
+
+//--------------------------------------------------------------
+void tool::saveData()
+{
+	if (mp_canvas){
+		mp_canvas->saveSettings(getDataPath());
+	}
+}
+
+//--------------------------------------------------------------
+void tool::loadData()
+{
+	if (mp_canvas){
+		mp_canvas->loadSettings(getDataPath());
+	}
+}
+
+//--------------------------------------------------------------
+string tool::getDataPath()
+{
+	return mp_toolManager->m_relPathData+"/"+getDataFileName();
+}
+//--------------------------------------------------------------
+bool tool::isHit(int x, int y)
+{
+	if (mp_canvas)
+		return mp_canvas->isHit(x,y);
+	return false;
+}
+
+//--------------------------------------------------------------
+void tool::enableDrawCallback(bool is)
+{
+	if (mp_canvas)
+	{
+		if (is){
+			mp_canvas->enableAppDrawCallback();
+			mp_canvas->enableMouseEventCallbacks();
+		}
+		else{
+			mp_canvas->disableAppDrawCallback();
+			mp_canvas->disableMouseEventCallbacks();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+toolManager::toolManager()
+{
+	mp_toolTab = 0;
+	mp_toolCurrent = 0;
+	m_relPathData = "Gui/tools";
+}
+
+//--------------------------------------------------------------
+toolManager::~toolManager()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt){
+		delete mapToolsIt->second;
+	}
+	delete mp_toolTab;
+	mp_toolTab = 0;
+}
+
+
+//--------------------------------------------------------------
+void toolManager::setLogo(string logoName)
+{
+    m_logo.loadImage(this->m_relPathData+"/"+logoName);
+}
+
+//--------------------------------------------------------------
+void toolManager::addTool(tool* pTool)
+{
+	m_mapTools[pTool->m_id] = pTool;
+}
+
+//--------------------------------------------------------------
+tool* toolManager::getTool(string id)
+{
+	return m_mapTools[id];
+}
+
+//--------------------------------------------------------------
+tool* toolManager::setTool(string id)
+{
+	if (m_mapTools.count(id) == 0) return 0;
+	mp_toolCurrent = 0;
+
+	hideAllTools();
+	mp_toolCurrent = m_mapTools[id];
+	mp_toolCurrent->show();
+	
+	return mp_toolCurrent;
+}
+
+//--------------------------------------------------------------
+tool* toolManager::selectTool(string id)
+{
+	setTool(id);
+	if (mp_radioTabs)
+		mp_radioTabs->activateToggle(id);
+	return mp_toolCurrent;
+}
+
+//--------------------------------------------------------------
+void toolManager::createControls(ofVec2f posCanvas, ofVec2f dimCanvas)
+{
+//    m_logo.loadImage(this->m_relPathData+"/ARicon_150x150.png");
+
+	vector<string> toolIds;
+
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		toolIds.push_back(pTool->m_id);
+	
+		pTool->createControls(m_fontName, posCanvas, dimCanvas);
+	}
+
+	mp_toolTab = new ofxUICanvas(0,0,400,400);
+	mp_toolTab->setFont(m_fontName);
+
+	mp_toolTab->setWidgetFontSize(OFX_UI_FONT_MEDIUM);
+	if (m_logo.isAllocated())
+	    mp_toolTab->addWidgetDown(new ofxUIImage(0,0,m_logo.getWidth(),m_logo.getHeight(),&m_logo,"logo",false));
+    mp_toolTab->addWidgetDown(new ofxUIFPS(OFX_UI_FONT_SMALL));
+	ofxUIRadio* mp_radioTabs = mp_toolTab->addRadio("Tabs", toolIds, OFX_UI_ORIENTATION_VERTICAL, OFX_UI_FONT_MEDIUM);
+	mp_toolTab->autoSizeToFitWidgets();
+
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		pTool->getCanvas()->setPosition(mp_toolTab->getRect()->getX()+10+mp_toolTab->getRect()->getWidth(), mp_toolTab->getRect()->getY());
+		pTool->createControlsCustomFinalize();
+		pTool->hide();
+	}
+
+   ofAddListener(mp_toolTab->newGUIEvent, this, &toolManager::handleEvents);
+   mp_toolTab->setTriggerWidgetsUponLoad(true);
+   mp_toolTab->disableAppDrawCallback();
+}
+
+//--------------------------------------------------------------
+void toolManager::loadData()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		pTool->loadData();
+	}
+
+	if (mp_toolTab)
+		mp_toolTab->loadSettings(getDataPath());
+}
+
+//--------------------------------------------------------------
+void toolManager::saveData()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		if (pTool)
+			pTool->saveData();
+	}
+	
+	if (mp_toolTab)
+		mp_toolTab->saveSettings(getDataPath());
+}
+
+
+//--------------------------------------------------------------
+void toolManager::setup()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		pTool->setup();
+	}
+}
+
+//--------------------------------------------------------------
+void toolManager::hideAllTools()
+{
+   map<string, tool*>::iterator mapToolsIt;
+   
+   // Hide all
+   mp_toolCurrent = 0;
+   for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+   {
+	   tool* pTool = mapToolsIt->second;
+	   if (pTool)
+	   {
+		   pTool->hide();
+		}
+   }
+}
+
+//--------------------------------------------------------------
+void toolManager::handleEvents(ofxUIEventArgs& e)
+{
+    string name = e.widget->getName();
+	// ofLog() << name;
+	if (name == "Tabs")
+	{
+        ofxUIRadio *radio = (ofxUIRadio *) e.widget;
+		string canvasId = radio->getActiveName();
+		setTool(canvasId);
+	}
+}
+
+//--------------------------------------------------------------
+void toolManager::enableDrawCallback(bool is)
+{
+	if (mp_toolCurrent)
+		mp_toolCurrent->enableDrawCallback(is);
+}
+
+
+//--------------------------------------------------------------
+void toolManager::updateUI()
+{
+	
+}
+
+//--------------------------------------------------------------
+void toolManager::update()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		pTool->update();
+	}
+}
+
+//--------------------------------------------------------------
+void toolManager::drawUI()
+{
+	if (mp_toolTab)
+		mp_toolTab->draw();
+	
+	if (mp_toolCurrent)
+		mp_toolCurrent->drawUI();
+}
+
+//--------------------------------------------------------------
+void toolManager::draw()
+{
+	if (mp_toolCurrent)
+		mp_toolCurrent->draw();
+}
+
+//--------------------------------------------------------------
+bool toolManager::isHit(int x, int y)
+{
+	if (mp_toolTab->isHit(x, y))
+		return true;
+
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		if (pTool->isHit(x, y))
+			return true;
+	}
+	
+	return false;
+}
+
+
+//--------------------------------------------------------------
+void toolManager::mousePressed(int x, int y, int button)
+{
+	if (mp_toolCurrent)
+		mp_toolCurrent->mousePressed(x,y,button);
+}
+
+//--------------------------------------------------------------
+bool toolManager::keyPressed(int key)
+{
+	if (mp_toolCurrent)
+		return mp_toolCurrent->keyPressed(key);
+	return false;
+}
+
+//--------------------------------------------------------------
+void toolManager::dragEvent(ofDragInfo dragInfo)
+{
+	if (mp_toolCurrent)
+		mp_toolCurrent->dragEvent(dragInfo);
+}
+
+//--------------------------------------------------------------
+void toolManager::windowResized(int w, int h)
+{
+	if (mp_toolCurrent)
+		mp_toolCurrent->windowResized(w,h);
+}
+
+//--------------------------------------------------------------
+void toolManager::exit()
+{
+	map<string, tool*>::iterator mapToolsIt;
+	for (mapToolsIt = m_mapTools.begin(); mapToolsIt != m_mapTools.end(); ++mapToolsIt)
+	{
+		tool* pTool = mapToolsIt->second;
+		pTool->exit();
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ src/tool.h
@@ -0,0 +1,105 @@
+//
+//  tool.h
+//  A_P_P_A_R_E_L
+//
+//  Created by Julien on 30/05/2014.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+#include "ofxUI.h"
+
+class toolManager;
+class tool
+{
+	public:
+		tool			(string id, toolManager* parent);
+		virtual ~tool	();
+
+				void	createControls		(string fontName, ofVec2f posCanvas, ofVec2f dimCanvas);
+		virtual	void	createControlsCustom(){}
+		virtual	void	createControlsCustomFinalize(){}
+		virtual void	updateControls		(){}
+		virtual	void	handleEvents		(ofxUIEventArgs& e){}
+		virtual	void	saveData			();
+		virtual	void	loadData			();
+		virtual	void	setup				(){}
+		virtual void	update				(){}
+		virtual void	drawUI				(){}
+		virtual void	draw				(){}
+		virtual	void	show				(bool is=true){if (mp_canvas) mp_canvas->setVisible(is);}
+				void	hide				(){show(false);}
+		virtual	bool	isHit				(int x, int y);
+		virtual	void	enableDrawCallback	(bool is=true);
+		virtual	void	exit				(){}
+
+	
+		virtual	void	mousePressed		(int x, int y, int button){}
+		virtual	bool	keyPressed			(int key){return false;}
+		virtual	void 	dragEvent			(ofDragInfo dragInfo){}
+	
+		virtual void	windowResized		(int w, int h){};
+
+
+		ofxUICanvas*	getCanvas()			{return mp_canvas;}
+		string			getControlId		(string which){return which+"_"+m_id;}
+		string			_id					(string which){return getControlId(which);}
+		string			getDataFileName		(){return m_id+".xml";}
+		string			getDataPath			();
+
+		string			m_id;
+		toolManager*	mp_toolManager;
+		ofxUICanvas*	mp_canvas;
+};
+
+
+class toolManager
+{
+	public:
+		toolManager			();
+		~toolManager		();
+	
+	
+		void				setLogo				(string logoName);
+		void				setFontName			(string fontName){m_fontName = fontName;}
+	
+		void				addTool				(tool*);
+		tool*				getTool				(string id);
+		tool*				setTool				(string id);
+		tool*				selectTool			(string id);
+		void				hideAllTools		();
+		void				createControls		(ofVec2f posCanvas, ofVec2f dimCanvas);
+		void				updateControls		(){}
+		void				loadData			();
+		void				saveData			();
+		void				setup				();
+		void				handleEvents		(ofxUIEventArgs& e);
+		void				update				();
+		void				updateUI			();
+		void				drawUI				();
+		void				draw				();
+		bool				isHit				(int x, int y);
+		void				enableDrawCallback	(bool is=true);
+		void				exit				();
+
+		string				getDataFileName		(){return "manager.xml";}
+		string				getDataPath			(){return m_relPathData+"/"+getDataFileName();}
+
+		void				mousePressed		(int x, int y, int button);
+		bool				keyPressed			(int key);
+		void 				dragEvent			(ofDragInfo dragInfo);
+	
+		void				windowResized		(int w, int h);
+
+		map<string, tool*>	m_mapTools;
+		string				m_relPathData;
+		ofxUICanvas*		mp_toolTab;
+		ofxUIRadio*			mp_radioTabs;
+		tool*				mp_toolCurrent;
+	
+		ofImage				m_logo;
+		string				m_fontName;
+	
+	
+};
